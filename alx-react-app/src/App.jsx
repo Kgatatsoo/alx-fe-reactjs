bf78f940a9628b8bcf28e42f396c412e490ba21c
kgatatsoo <vibrationbanda@gmail.com>
2025-02-17 00:01:50 +0200

    added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/footer';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -8,10 +11,13 @@ import './App.css'
 function App() {
   return (
       <div className="App">
-          <WelcomeMessage />
+          <Header />
+          <MainContent />
+          <Footer />
       </div>
   );
 }
+
 
 export default App;
 
