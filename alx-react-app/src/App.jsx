5baf6e129c645a0e81c28b826fdeda50135e1dec
kgatatsoo <vibrationbanda@gmail.com>
2025-02-17 00:18:39 +0200

    added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,8 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/footer';
+import UserProfile from './components/UserProfile';
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -10,14 +12,15 @@ import './App.css'
 
 function App() {
   return (
-      <div className="App">
-          <Header />
-          <MainContent />
-          <Footer />
-      </div>
+    <div className="App">
+      <UserProfile 
+        name="LERATO" 
+        age="29" 
+        bio="MUSIC LOVER" 
+      />
+    </div>
   );
 }
-
 
 export default App;
 
