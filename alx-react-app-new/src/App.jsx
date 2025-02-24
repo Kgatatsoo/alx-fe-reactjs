5f2ae75484b42d3c3cb70900f8ed4c23033866d5
kgatatsoo <vibrationbanda@gmail.com>
2025-02-24 22:02:24 +0200

    added

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,28 +1,16 @@
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/footer';
-import UserProfile from './components/UserProfile';
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-
+import React from 'react';
+import Counter from './components/Counter'; 
 
 function App() {
   return (
-    <div className="App">
-      <UserProfile 
-        name="Alice" 
-        age="25" 
-        bio="Loves hiking and photography" 
-      />
+    <div>
+      <h1>React Counter Application</h1>
+      <Counter /> {/* Include the Counter component here */}
     </div>
   );
 }
 
 export default App;
+
 
 
