7f1859b34479f0f90f962e7f950640a42e5243c2
kgatatsoo <vibrationbanda@gmail.com>
2025-02-24 21:05:02 +0200

    changes

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 const UserProfile = (props) => {
     return (
-      <div>
-        <h2>{props.name}</h2>       {/* Display the user's name */}
-        <p>Age: {props.age}</p>    {/* Display the user's age */}
-        <p>Bio: {props.bio}</p>    {/* Display the user's bio */}
-      </div>
+      <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+   <h2 style={{ color: 'blue' }}>{props.name}</h2>
+   <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+   <p>Bio: {props.bio}</p>
+ </div>
     );
   };
   
