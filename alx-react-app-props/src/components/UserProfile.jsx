88d27d98340152cab6b2c9745218b3e43c18914e
kgatatsoo <vibrationbanda@gmail.com>
2025-02-27 21:11:52 +0200

    added

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,18 @@
-const UserProfile = (props) => {
-    return (
-      <div>
-        <h2>{props.name}</h2>       {/* Display the user's name */}
-        <p>Age: {props.age}</p>    {/* Display the user's age */}
-        <p>Bio: {props.bio}</p>    {/* Display the user's bio */}
-      </div>
-    );
-  };
-  
-  export default UserProfile;
-  
+
+
+import React, { useContext } from 'react'; 
+import UserContext from '../UserContext'; 
+
+function UserProfile() {
+  const userData = useContext(UserContext); 
+
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
