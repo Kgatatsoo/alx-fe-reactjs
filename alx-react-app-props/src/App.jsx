6927e5457fb8899afe9c664aa178ff18d0314ed5
kgatatsoo <vibrationbanda@gmail.com>
2025-02-27 21:07:46 +0200

    added

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,28 +1,18 @@
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/footer';
-import UserProfile from './components/UserProfile';
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 
+import React from 'react';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext'; 
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
-    <div className="App">
-      <UserProfile 
-        name="Alice" 
-        age="25" 
-        bio="Loves hiking and photography" 
-      />
-    </div>
+  
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
 export default App;
-
-
---
+++ alx-react-app-props/src/components/UserContext.js
@@ -0,0 +1,9 @@
+// src/UserContext.js
+
+import React from 'react';
+
+// Creating a UserContext to manage the user data across components
+const UserContext = React.createContext();
+
+// Exporting the context for use in other components
+export default UserContext;
