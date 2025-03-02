75f1f49c64f9e054b86b9b4ad6be1985b365f670
kgatatsoo <vibrationbanda@gmail.com>
2025-03-02 21:04:36 +0200

    added new directory

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import AddRecipeForm from './AddRecipeForm';
+import RecipeList from './RecipeList';
+
+function App() {
+  return (
+    <div>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
