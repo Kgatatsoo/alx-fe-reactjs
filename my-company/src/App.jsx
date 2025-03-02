c4211fedde158f26c63b578842d5eb9a23ecb797
kgatatsoo <vibrationbanda@gmail.com>
2025-03-02 20:20:36 +0200

    added

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,20 +1,27 @@
+
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import Navbar from './components/Navbar'; 
+import Home from './pages/Home';  
+import About from './pages/About';  
+import Services from './pages/Services';  
+import Contact from './pages/Contact';  
+import Footer from './components/Footer';  
 
- 
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-
-const App = () => {
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <Navbar /> {m}
+      <div style={{ padding: '20px' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+      <Footer /> {m}
+    </Router>
   );
 };
 
