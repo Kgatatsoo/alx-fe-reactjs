b91812786356bc47bf50b9e5c691267db5e7a6e5
kgatatsoo <vibrationbanda@gmail.com>
2025-03-04 23:00:42 +0200

    added

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
+import Profile from './components/Profile';
 import BlogPost from './components/BlogPost'; 
 
 const App = () => {
