16406ee4eb62b8f60674e84323f9d87f2d05a755
kgatatsoo <vibrationbanda@gmail.com>
2025-03-04 22:05:50 +0200

    added

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,4 +1,7 @@
+
+import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import PostsComponent from './components/PostsComponent';
 
 const queryClient = new QueryClient();
 
@@ -9,6 +12,9 @@ function App() {
     </QueryClientProvider>
   );
 }
+
+
+
 
 export default App;
 
