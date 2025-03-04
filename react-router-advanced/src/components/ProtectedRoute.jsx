e3f02cfa15a3d9661a0da08169889c71c9f2e4b6
kgatatsoo <vibrationbanda@gmail.com>
2025-03-04 22:55:44 +0200

    added

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,13 +1,17 @@
 
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth'; 
 
-function ProtectedRoute({ children }) {
-  const isAuthenticated = false; 
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+
   if (!isAuthenticated) {
-    return <Navigate to="/" />;
+    
+    return <Navigate to="/login" />;
   }
-  return children;
-}
+
+  return children; 
+};
 
 export default ProtectedRoute;
