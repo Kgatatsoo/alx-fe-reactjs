4e4666c1c0938c45df552b49e2defd4228b523e1
kgatatsoo <vibrationbanda@gmail.com>
2025-03-04 22:09:45 +0200

    added

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -16,21 +16,6 @@ function App() {
 
 
 
-export default App;
 
-import React from 'react';
-import './App.css';
-import FormikForm from './components/formikForm';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to the User Registration Form</h1>
-      <FormikForm />
-    </div>
-  );
-}
-
-export default App;
 
 
