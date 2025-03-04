486e60a537d68deac2048e30e3dc49d83b5f8026
kgatatsoo <vibrationbanda@gmail.com>
2025-03-04 21:43:09 +0200

    added

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -5,13 +5,11 @@ import * as Yup from 'yup';
 
 const FormikForm = () => {
   const validationSchema = Yup.object({
-    username: Yup.string()
-      .required('Username is required'),
+    username: Yup.string().required('Username is required'),
     email: Yup.string()
       .email('Invalid email format')
       .required('Email is required'),
-    password: Yup.string()
-      .required('Password is required')
+    password: Yup.string().required('Password is required')
       .min(6, 'Password must be at least 6 characters'),
   });
 
